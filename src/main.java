1899ab794365a9e869d8bfb789d222b861a5c28a
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2021-11-28 22:36:07 +0300

    Added src/

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+       int currentBalance = 2000000000;
+       int transferAmount = 500000000;
+       int totalSum = currentBalance + transferAmount;
+       System.out.println(totalSum);
+    }
+}
+
+
